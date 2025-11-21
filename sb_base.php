7bd56d10441b981cffa53b44534c0342cc6de592
CHUA ZHUN YU <chuazy-wm23@student.tarc.edu.my>
2025-11-21 20:24:23 +0800

    Add general page functions and error handling

--- sb_base.php
+++ sb_base.php
@@ -15,4 +15,77 @@ try{
 }catch(PDOException $e){
     die("Connection failed: ".$e->getMessage());
 }
+
+
+
+
+
+
+
+
+
+    
+// ============================================================================
+// General Page Functions
+// ============================================================================
+
+// Is GET request
+function is_get() {
+    return $_SERVER['REQUEST_METHOD'] === 'GET';
+}
+
+// Is POST request
+function is_post() {
+    return $_SERVER['REQUEST_METHOD'] === 'POST';
+}
+
+// Get GET parameter
+function get($key, $value = null) {
+    $value = $_GET[$key] ?? $value;
+    return is_array($value) ? array_map('trim', $value) : trim($value);
+}
+
+// Get POST parameter
+function post($key, $value = null) {
+    $value = $_POST[$key] ?? $value;
+    return is_array($value) ? array_map('trim', $value) : trim($value);
+}
+
+// Get REQUEST parameter
+function req($key, $value = null) {
+    $value = $_REQUEST[$key] ?? $value;
+    return is_array($value) ? array_map('trim', $value) : trim($value);
+}
+
+// Redirect
+function redirect($url = null) {
+    $url ??= $_SERVER['REQUEST_URI'];
+    header("Location: $url");
+    exit();
+}
+
+// Temporary session variable (flash message)
+function temp($key, $value = null) {
+    if ($value !== null) {
+        $_SESSION["temp_$key"] = $value;
+    } else {
+        $value = $_SESSION["temp_$key"] ?? null;
+        unset($_SESSION["temp_$key"]);
+        return $value;
+    }
+}
+
+
+// ============================================================================
+// Error Handling
+// ============================================================================
+$_err = [];
+function err($key) {
+    global $_err;
+    if (!empty($_err[$key])) {
+        echo "<span class='err'>{$_err[$key]}</span>";
+    } else {
+        echo "<span></span>";
+    }
+}
 ?>
