fce3554c3d22df97938d7724dd93f807032831b7
JohnyChia <johnychia731@gmail.com>
2025-11-29 10:21:21 +0800

    commit

--- page/adminPanel.php
+++ page/adminPanel.php
@@ -3,6 +3,7 @@ include 'admin_sidebar.php';
 ?>
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -10,6 +11,7 @@ include 'admin_sidebar.php';
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
      <link rel="stylesheet" href="/css/admin.css">
 </head>
+
 <body>
     <!-- Main Content -->
     <div class="main-content">
@@ -137,5 +139,4 @@ include 'admin_sidebar.php';
 
     </div>
 </body>
->>>>>>> Stashed changes
 </html>
