ebfca2a41f533a5285ef9ca631b8c9861593465c
JohnyChia <johnychia731@gmail.com>
2025-11-29 10:22:15 +0800

    Update adminPanel.php

--- page/adminPanel.php
+++ page/adminPanel.php
@@ -9,7 +9,7 @@ include 'admin_sidebar.php';
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>SB Online | Admin Panel</title>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
-     <link rel="stylesheet" href="/css/admin.css">
+    <link rel="stylesheet" href="/css/admin.css">
 </head>
 
 <body>
@@ -139,4 +139,5 @@ include 'admin_sidebar.php';
 
     </div>
 </body>
+
 </html>
